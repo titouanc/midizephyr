747c1e9a449d9be7f7a5db71dc443afbc8f040e4
iTitou <moiandme@gmail.com>
2021-09-12 13:36:53 +0200

    Update with pad touch
    Signed-off-by: iTitou <moiandme@gmail.com>

--- kinesta/src/main.c
+++ kinesta/src/main.c
@@ -11,11 +11,14 @@ LOG_MODULE_REGISTER(app);
 
 const rgb_pad pad = RGB_PAD_DT_SPEC(pad);
 
+const int threshold_cm = 75;
+
 void main(void)
 {
     int distance_cm, color_intensity;
     struct sensor_value value;
     const struct device *sensor = device_get_binding("VL53L0X_C");
+
     if (! sensor){
         return;
     }
@@ -34,11 +37,15 @@ void main(void)
             return;
         }
         distance_cm = 100 * sensor_value_to_double(&value);
-        if (distance_cm < 64){
-            color_intensity = (64 - distance_cm) << 4;
-            set_pad_color(&pad, COLOR_RGB(0, 0, color_intensity));
+
+
+        if (gpio_pin_get_dt(&pad.out) > 0){
+            set_pad_color(&pad, COLOR_RGB(color_intensity, color_intensity, 0));
+        } else if (distance_cm < threshold_cm){
+            color_intensity = (threshold_cm - distance_cm) * COLOR_CHAN_MAX / threshold_cm;
+            set_pad_color(&pad, COLOR_RGB(color_intensity, color_intensity, color_intensity));
         } else {
-            set_pad_color(&pad, COLOR_RGB(0, 0, 0));
+            set_pad_color(&pad, COLOR_RGB(COLOR_CHAN_MAX / 64, 0, 0));
         }
     }
 }
