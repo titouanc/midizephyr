d0718f94659762f34c755b1ba54d957f3977513c
iTitou <moiandme@gmail.com>
2021-09-23 21:04:31 +0200

    Add notion of tracking zone and colormap red-green for distance visual feedback
    Signed-off-by: iTitou <moiandme@gmail.com>

--- kinesta/src/main.c
+++ kinesta/src/main.c
@@ -11,13 +11,13 @@ LOG_MODULE_REGISTER(app);
 
 const rgb_pad pad = RGB_PAD_DT_SPEC(pad);
 
-const int threshold_cm = 75;
+const int THRESHOLD_CM = 60;
 
 void main(void)
 {
-    int distance_cm, color_intensity;
+    int distance_cm, measured_distance_cm, color_intensity;
     struct sensor_value value;
-    const struct device *sensor = device_get_binding("VL53L0X_C");
+    const struct device *sensor = device_get_binding("VL53L0X_L");
 
     if (! sensor){
         return;
@@ -28,6 +28,8 @@ void main(void)
 
     set_pad_color(&pad, COLOR_RGB(0, 0, 0));
 
+    bool is_in_tracking_zone = false;
+
     while (1){
         if (sensor_sample_fetch(sensor)){
             return;
@@ -36,16 +38,32 @@ void main(void)
         if (sensor_channel_get(sensor, SENSOR_CHAN_DISTANCE, &value)){
             return;
         }
-        distance_cm = 100 * sensor_value_to_double(&value);
+        measured_distance_cm = 100 * sensor_value_to_double(&value);
 
+        if (is_in_tracking_zone){
+            distance_cm = (measured_distance_cm + 3*distance_cm) / 4;
+        } else {
+            distance_cm = measured_distance_cm;
+        }
+
+        bool is_now_in_tracking_zone = false;
+        if (is_in_tracking_zone){
+            is_now_in_tracking_zone = distance_cm < THRESHOLD_CM;
+        } else {
+            is_now_in_tracking_zone = distance_cm < THRESHOLD_CM - 5;
+        }
+        is_in_tracking_zone = is_now_in_tracking_zone;
 
         if (gpio_pin_get_dt(&pad.out) > 0){
             set_pad_color(&pad, COLOR_RGB(color_intensity, color_intensity, 0));
-        } else if (distance_cm < threshold_cm){
-            color_intensity = (threshold_cm - distance_cm) * COLOR_CHAN_MAX / threshold_cm;
-            set_pad_color(&pad, COLOR_RGB(color_intensity, color_intensity, color_intensity));
+        } else if (is_in_tracking_zone){
+            int red = (THRESHOLD_CM - distance_cm) * COLOR_CHAN_MAX / THRESHOLD_CM;
+            int green = distance_cm * COLOR_CHAN_MAX / THRESHOLD_CM;
+            set_pad_color(&pad, COLOR_RGB(red, green, 0));
+        } else if (distance_cm < 150) {
+            set_pad_color(&pad, COLOR_RGB(0, 0, COLOR_CHAN_MAX/4));
         } else {
-            set_pad_color(&pad, COLOR_RGB(COLOR_CHAN_MAX / 64, 0, 0));
+            set_pad_color(&pad, COLOR_RGB(0, 0, 0));
         }
     }
 }
