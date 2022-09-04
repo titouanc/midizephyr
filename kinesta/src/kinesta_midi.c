55c644871ffab41850ce1f14f32e05121a48b2ff
iTitou <moiandme@gmail.com>
2022-09-04 12:24:55 +0200

    Prepare kinesta_midi for individual MIDI output switching
    Signed-off-by: iTitou <moiandme@gmail.com>

--- kinesta/src/kinesta_midi.c
+++ kinesta/src/kinesta_midi.c
@@ -4,14 +4,21 @@
 
 #include <drivers/uart.h>
 
+bool kinesta_midi_usb_enable = true;
+bool kinesta_midi_din_enable = true;
+
 const struct device *midi_din_uart = DEVICE_DT_GET(DT_NODELABEL(midi_din_uart));
 
 void kinesta_midi_out(const uint8_t midi_pkt[3])
 {
-    uint8_t midi_cmd = midi_pkt[0] >> 4;
-    for (size_t i=0; i<midi_datasize(midi_cmd); i++){
-        uart_poll_out(midi_din_uart, midi_pkt[i]);
+    if (kinesta_midi_din_enable) {
+        uint8_t midi_cmd = midi_pkt[0] >> 4;
+        for (size_t i=0; i<midi_datasize(midi_cmd); i++){
+            uart_poll_out(midi_din_uart, midi_pkt[i]);
+        }
     }
 
-    usb_midi_write(USB_MIDI_SENSORS_JACK_ID, midi_pkt);
+    if (kinesta_midi_usb_enable) {
+        usb_midi_write(USB_MIDI_SENSORS_JACK_ID, midi_pkt);
+    }
 }
