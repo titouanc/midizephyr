6efff0a9ee9bea719cc102803e3cea4910abed52
iTitou <moiandme@gmail.com>
2022-10-15 12:46:43 +0200

    New "driver" kinesta midi DIN
    Signed-off-by: iTitou <moiandme@gmail.com>

--- kinesta/src/kinesta_midi.c
+++ kinesta/src/kinesta_midi.c
@@ -3,22 +3,52 @@
 #include "usb_midi.h"
 
 #include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/gpio.h>
 
-bool kinesta_midi_usb_enable = true;
-bool kinesta_midi_din_enable = true;
+struct kinesta_midi_din {
+    bool enabled;
+    const struct device *uart;
+    const struct gpio_dt_spec rx_led;
+    const struct gpio_dt_spec tx_led;
+};
 
-const struct device *midi_din_uart = DEVICE_DT_GET(DT_NODELABEL(midi_din_uart));
+#define KMIDI_FROM_DT(node) \
+    {.enabled=true, .uart=DEVICE_DT_GET(DT_PROP(node, uart)), \
+     .rx_led=GPIO_DT_SPEC_GET(node, rx_led_gpios), \
+     .tx_led=GPIO_DT_SPEC_GET(node, tx_led_gpios)},
+
+#define N_MIDI_DINS ARRAY_SIZE(midi_dins)
+
+static struct kinesta_midi_din midi_dins[] = {
+    DT_FOREACH_STATUS_OKAY(kinesta_midi_din, KMIDI_FROM_DT)
+};
+
+static void kinesta_midi_din_transmit(struct kinesta_midi_din *self, const uint8_t midi_pkt[3]) {
+    if (! self->enabled){
+        gpio_pin_set_dt(&self->tx_led, 0);
+        gpio_pin_set_dt(&self->rx_led, 0);
+        return;
+    }
+
+    gpio_pin_set_dt(&self->tx_led, 1);
+
+    uint8_t midi_cmd = midi_pkt[0] >> 4;
+    for (size_t i=0; i<midi_datasize(midi_cmd); i++){
+        uart_poll_out(self->uart, midi_pkt[i]);
+    }
+
+    gpio_pin_set_dt(&self->tx_led, 0);
+}
+
+static bool kinesta_midi_usb_enabled = true;
 
 void kinesta_midi_out(const uint8_t midi_pkt[3])
 {
-    if (kinesta_midi_din_enable) {
-        uint8_t midi_cmd = midi_pkt[0] >> 4;
-        for (size_t i=0; i<midi_datasize(midi_cmd); i++){
-            uart_poll_out(midi_din_uart, midi_pkt[i]);
-        }
+    for (size_t i=0; i<N_MIDI_DINS; i++){
+        kinesta_midi_din_transmit(&midi_dins[i], midi_pkt);
     }
 
-    if (kinesta_midi_usb_enable) {
+    if (kinesta_midi_usb_enabled) {
         usb_midi_write(USB_MIDI_SENSORS_JACK_ID, midi_pkt);
     }
 }
